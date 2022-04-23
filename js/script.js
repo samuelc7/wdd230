e7988c063b7325273b5b98ebd46e96d4b238371a
unknown <cummings.samuel007@gmail.com>
2022-04-23 12:12:31 -0600

    added lesson links template

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
 document.getElementById("last-modified").textContent = document.lastModified;
-
-
-const todaysDate = new Date();
-const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options)
