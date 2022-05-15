ee4a049cc3352ef4988d726acd2f5147bef023b9
samuelc7 <cummings.samuel007@gmail.com>
2022-05-14 23:34:58 -0600

    week4 assignment

---
+++ chamber/js/main.js
@@ -0,0 +1,35 @@
+var element = document.getElementById("time");
+var date = new Date();
+var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday", 
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+var formattedDate = `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+element.innerHTML = formattedDate;
+
+var button = document.getElementById("hamburger");
+button.style.display = "block";
+var nav = document.querySelector("nav");
+button.addEventListener("click", ()=>  {
+    nav.classList.toggle('responsive')},
+    false);
