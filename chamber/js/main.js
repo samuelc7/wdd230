82c0901c8fc3a91e5d4f135d635caa82d063b0f3
unknown <cummings.samuel007@gmail.com>
2022-05-22 15:54:53 -0600

    fixed menu button

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -28,13 +28,53 @@ var formattedDate = `${days[date.getDay()]}, ${date.getDate()} ${months[date.get
 element.innerHTML = formattedDate;
 element.style.color = "white";
 
-const button = document.getElementById("hamburger");
-button.style.display = "none";
+// button.style.display = "none";
 
-window.onresize = ()=> {
-    if (window.innerWidth < 800) 
-    button.style.display = "block";
-    if (window.innerWidth > 800) 
-    button.style.display = "none";
-};
+// const menuList = document.querySelector(".menu");
 
+// window.onresize = ()=> {
+//     if (window.innerWidth < 800) 
+//     button.style.display = "block";
+//     if (window.innerWidth > 800) 
+//     button.style.display = "none";
+// };
+
+// var hamMenu;
+
+// if (window.innerWidth > 800) {
+//     if (menuList.children.length == 5) {
+//         // remove the hamburger item
+//         hamMenu = menuList.children[0];
+//         menuList.removeChild(menuList.children[0]);
+//     }
+// } else {
+//     console.log("here");
+//     menuList.appendChild(hamMenu);
+// }
+
+const menu = document.querySelector(".menu");
+console.log(menu.style);
+
+
+var shown = false;
+function showMenu() {
+    if (shown) {
+        menu.children[0].style = "display: block; margin-top: 1rem;"
+        menu.style.display = "flex";
+        menu.style.position = "absolute";
+        menu.style.marginTop = "3px";
+        for (var i = 1; i < menu.children.length; i++) {
+            menu.children[i].style.display = "none";
+        }
+        shown = false;
+    } else {
+        menu.style = "position: absolute;margin-top: 6rem;"
+        menu.children[0].style = "display: block;position: absolute;margin-top: -4rem;margin-left: 2rem;"
+        menu.style.display = "flex";
+        menu.style.flexDirection = "column";
+        for (var i = 1; i < menu.children.length; i++) {
+            menu.children[i].style.display = "contents";
+        }
+        shown = true;
+    }
+}
