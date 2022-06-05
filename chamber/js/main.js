5e741ae48b1879caeedc958c6e8f6dd3e3eed4a9
unknown <cummings.samuel007@gmail.com>
2022-06-04 21:51:47 -0600

    added discover page

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -89,11 +89,44 @@ function showMenu() {
 
 // Only show the join button on mondays and tuesdays only
 const joinButton = document.querySelector(".join")
-if (days[date.getDay()] == "Monday" || days[date.getDay()] == "Tuesday") {
-    joinButton.style.display = "block";
-} else {
-    joinButton.style.display = "none";
+if (joinButton) {
+    if (days[date.getDay()] == "Monday" || days[date.getDay()] == "Tuesday") {
+        joinButton.style.display = "block";
+    } else {
+        joinButton.style.display = "none";
+    }
 }
 function toJoinField() {
     console.log("in function")
 }
+
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
