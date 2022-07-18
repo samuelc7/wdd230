28a11e9460063a345fa9d1b730bf65162f11b9b9
unknown <cummings.samuel007@gmail.com>
2022-07-18 00:04:51 -0600

    final pushes for the night

--- final/js/main.js
+++ final/js/main.js
@@ -124,31 +124,46 @@ function pullForm() {
 }
 
 
-// Fill Weather values
 let key = "36b5b7d508567f61ed1b21c343adb785";
-var weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=38.9&lon=77&appid=${key}`;
-        fetch(weather_url)
-            .then((response) => {
-                return response.json();
-            })
-            .then((jsonObject) => {
-                const weather = jsonObject["weather"][0];
-                const main = jsonObject["main"];
-                const wind = jsonObject["wind"];
+var cord_url = `https://api.openweathermap.org/geo/1.0/direct?q=washington&limit=2&appid=${key}`;
+let long;
+let lat;
 
-                const icon = weather["icon"];
-                const desc = weather["description"].charAt(0).toUpperCase() +  weather["description"].substring(1, weather["description"].length);
-                const humidity = main["humidity"];
-                const tempK = main["temp"];
-                const temp = Math.round(1.8*(tempK-273) + 32);
-                const windSpeed = wind["speed"];
+fetch(cord_url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((jsonObject) => {
+        long = jsonObject[0]["lon"];
+        lat = jsonObject[0]["lat"];
 
-                let todaysIcon = document.getElementById("todayImage");
-                let todaysTemp = document.getElementById("todayTemp");
-                let todayHumidity = document.getElementById("todayCondition");
+        // Fill Weather values
+        var weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+                fetch(weather_url)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((jsonObject) => {
+                        const weather = jsonObject["weather"][0];
+                        const main = jsonObject["main"];
+                        const wind = jsonObject["wind"];
 
-                todaysIcon.src = icon;
-            });
+                        const icon = weather["icon"];
+                        const desc = weather["description"].charAt(0).toUpperCase() +  weather["description"].substring(1, weather["description"].length);
+                        const humidity = main["humidity"];
+                        const tempK = main["temp"];
+                        const temp = Math.round(1.8*(tempK-273) + 32);
+                        const windSpeed = wind["speed"];
 
+                        let todaysIcon = document.getElementById("todayImage");
+                        let todaysTemp = document.getElementById("todayTemp");
+                        let todaysCondition = document.getElementById("todayCondition");
+                        let todaysHumidity = document.getElementById("todayHumidity");
 
-
+                        todaysIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+                        todaysTemp.innerText = temp;
+                        todaysCondition.innerText = desc;
+                        todaysHumidity.innerText = humidity;
+                    });
+                })
+                
